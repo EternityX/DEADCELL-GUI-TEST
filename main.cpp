e1605472a4f178cdb2e8fb75f29840bad2652102
eternity <eternity@enevade.org>
2021-07-10 03:36:07 -0700

    Slight cleanup.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <controls/button.h>
 #include <controls/checkbox.h>
 #include <controls/progressbar.h>
+#include <controls/spinner.h>
 
 static LPDIRECT3D9 g_d3d = nullptr;
 static LPDIRECT3DDEVICE9 g_d3d_device = nullptr;
@@ -68,8 +69,7 @@ bool create_device_d3d(HWND hWnd) {
     g_d3d_parameters.PresentationInterval = D3DPRESENT_INTERVAL_ONE; // Present with vsync
     //g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present without vsync, maximum unthrottled framerate
 
-    if (g_d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING,
-                            &g_d3d_parameters, &g_d3d_device) < 0) {
+    if (g_d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3d_parameters, &g_d3d_device) < 0) {
         return false;
     }
 
@@ -87,39 +87,6 @@ void cleanup_device_d3d() {
     }
 }
 
-auto window1 = std::make_shared<gui::window>("Window", "window_id1");
-auto window2 = std::make_shared<gui::window>("Non-resizeable window", "window_id2");
-auto window3 = std::make_shared<gui::window>("window_id3");
-
-auto button2 = std::make_shared<gui::button>("Auto sizing example", "button_id2", []() { });
-
-auto button3 = std::make_shared<gui::button>("Disabled button", "button_id3", []() { });
-
-bool test_var = false;
-auto checkbox1 = std::make_shared<gui::checkbox>("Checkbox", "checkbox_id1", &test_var, []()
-{
-    test_var = !test_var;
-});
-
-auto checkbox2 = std::make_shared<gui::checkbox>("Disabled checkbox", "checkbox_id2", &test_var, []()
-{
-    test_var = !test_var;
-});
-
-auto progress = std::make_shared<gui::progressbar<int>>("Progress bar", "progress_id1", 420.0f, []()
-{
-    std::cout << "progress completed\n";
-});
-
-auto button = std::make_shared<gui::button>("Increment by 25", "button_id1", []()
-{
-    button2->set_text("This is a really long message that will automatically wrap text and resize the button");
-    button2->set_auto_size(true);
-
-    progress->increment_progress(25);
-});
-
-
 int main() {
     WNDCLASSEX wc = {
         sizeof(WNDCLASSEX), CS_CLASSDC, wnd_proc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr,
@@ -128,8 +95,7 @@ int main() {
 
     RegisterClassEx(&wc);
 
-    HWND hwnd = CreateWindow(wc.lpszClassName, L"deadcell-gui-test", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL,
-                             NULL, wc.hInstance, NULL);
+    HWND hwnd = CreateWindow(wc.lpszClassName, L"deadcell-gui-test", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
 
     if (!create_device_d3d(hwnd)) {
         cleanup_device_d3d();
@@ -145,64 +111,83 @@ int main() {
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplDX9_Init(g_d3d_device);
 
-    const ImVec4 clear_color = ImVec4(0.29f, 0.31f, 0.36f, 1.00f);
+    const auto clear_color = ImVec4(0.29f, 0.31f, 0.36f, 1.00f);
 
-    auto inst = std::make_shared<gui::deadcell_gui>();
+    std::shared_ptr<gui::deadcell_gui> gui = gui::create();
 
+    auto window1 = gui->add<gui::window>("Window", "window_id1");
     window1->set_position_size({ 0, 0 }, { 250, 250 });
+
+    auto window2 = gui->add<gui::window>("Non-resizeable window", "window_id2");
     window2->set_position_size({ 200, 200 }, { 250, 250 });
-    window3->set_position_size({ 400, 400 }, { 250, 250 });
-
-    //auto meme = inst->add<gui::button>("hello", "hello", []() { std::cout << "pressed\n"; });
-
-    inst->wm()->add_window(window1);
-    inst->wm()->add_window(window2);
-    inst->wm()->add_window(window3);
-
-    inst->wm()->move_to_front(window1, true);
-
-    //window1->add_child(meme);
-
     window2->set_resizeable(false);
+
+    auto window3 = gui->add<gui::window>("window_id3");
+    window3->set_position_size({ 400, 400 }, { 250, 250 });
     window3->set_titlebar_height(24.0f);
 
-    button->set_position({ 150, 150 });
-    button->set_size({ 150, 35 });
-    button->set_auto_size(false);
+    gui->wm()->add_window(window1);
+    gui->wm()->add_window(window2);
+    gui->wm()->add_window(window3);
+    gui->wm()->move_to_front(window1, true);
 
-    button2->set_enabled(true);
-    button2->set_position({ 150, button->get_position().y + button->get_size().y + 8 });
-    button2->set_size({ 150, 35 });
-    button2->set_auto_size(false);
+    auto button2 = gui->add<gui::button>("Auto sizing example", "button_id2", []() {});
+    window1->add_child(button2);
 
-    button3->set_enabled(false);
-    button3->set_position({ 150, button2->get_position().y + button2->get_size().y + 8 });
-    button3->set_size({ 150, 35 });
-    button3->set_auto_size(false);
+    auto button3 = std::make_shared<gui::button>("Disabled button", "button_id3", []() {});
+    button2->add_child(button3);
+
+    bool test_var = false;
+    auto checkbox1 = gui->add<gui::checkbox>("Checkbox", "checkbox_id1", &test_var, [&]() {
+        test_var = !test_var;
+    });
 
     checkbox1->set_position({ 50, 80 });
+    window1->add_child(checkbox1);
+
+    auto checkbox2 = gui->add<gui::checkbox>("Disabled checkbox", "checkbox_id2", &test_var, [&]() {
+        test_var = !test_var;
+    });
 
     checkbox2->set_position({ 50, checkbox1->get_position().y + checkbox1->get_size().y + 8 });
     checkbox2->set_enabled(false);
-    
-    button2->add_child(button3);
-    window1->add_child(button2);
-    window1->add_child(button);
-    window1->add_child(checkbox1);
     window1->add_child(checkbox2);
+
+    auto progress = gui->add<gui::progressbar<int>>("Progress bar", "progress_id1", 420.0f, []() {
+        std::cout << "progress completed\n";
+    });
 
     progress->set_position({ 500, 500 });
     window1->add_child(progress);
-    auto str = button2->build_class_tree();
-    std::cout << str << "\n";
 
-    str = window1->build_class_tree();
-    std::cout << str << "\n";
+    auto button = std::make_shared<gui::button>("Increment by 25", "button_id1", [&]() {
+        button2->set_text("This is a really long message that will automatically wrap text and resize the button");
+        button2->set_auto_size(true);
+
+        progress->increment_progress(25);
+    });
+
+    window1->add_child(button);
+
+    button->set_position({ 150, 150 });
+    button->set_size({ 150, 35 });
+
+    button2->set_position({ 150, button->get_position().y + button->get_size().y + 8 });
+    button2->set_size({ 150, 35 });
+    
+    button3->set_enabled(false);
+    button3->set_position({ 150, button2->get_position().y + button2->get_size().y + 8 });
+    button3->set_size({ 150, 35 });
+
+    const auto spinner = gui->add<gui::spinner>("spinner_id1");
+    window1->add_child(spinner);
+    
+    std::cout << window1->build_class_tree() << "\n";
 
     bool done = false;
     while (!done) {
         MSG msg;
-        while (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
+        while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
             if (msg.message == WM_QUIT) {
@@ -216,18 +201,13 @@ int main() {
         ImGui_ImplDX9_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
+        gui::drawing::set_draw_list(gui::drawing::draw_list_foreground);
 
-        ImGui::GetBackgroundDrawList()->ChannelsSplit(8);
-        ImGui::GetForegroundDrawList()->ChannelsSplit(8);
+        gui->wm()->new_frame();
+        {
+            button3->set_position({ 150, button2->get_position().y + button2->get_size().y + 8 });
+        }
 
-        ImGui::GetForegroundDrawList()->ChannelsSetCurrent(0);
-        ImGui::GetBackgroundDrawList()->ChannelsSetCurrent(0);
-
-        set_draw_list(gui::drawing::draw_list_foreground);
-
-        inst->wm()->new_frame();
-        button3->set_position({ 150, button2->get_position().y + button2->get_size().y + 8 });
-        //ImGui::GetForegroundDrawList()->ChannelsMerge();
         ImGui::EndFrame();
 
         g_d3d_device->SetRenderState(D3DRS_ZENABLE, FALSE);
@@ -235,9 +215,9 @@ int main() {
         g_d3d_device->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
 
         const D3DCOLOR clear_col_dx = D3DCOLOR_RGBA(static_cast<int>(clear_color.x * clear_color.w * 255.0f),
-                                                    static_cast<int>(clear_color.y * clear_color.w * 255.0f),
-                                                    static_cast<int>(clear_color.z * clear_color.w * 255.0f),
-                                                    static_cast<int>(clear_color.w * 255.0f));
+            static_cast<int>(clear_color.y * clear_color.w * 255.0f),
+            static_cast<int>(clear_color.z * clear_color.w * 255.0f),
+            static_cast<int>(clear_color.w * 255.0f));
 
         g_d3d_device->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
         if (g_d3d_device->BeginScene() >= 0) {
